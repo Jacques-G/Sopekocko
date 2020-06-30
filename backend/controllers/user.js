7f45d64ca0a510da0bab57d42c0afa305ae864b2
Jacques Garcia <jack.garcia@outlook.fr>
2020-06-30 09:06:44 +0200

    modifs erreur 401 unauthorized

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -18,7 +18,7 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email : req.body.email })
+    User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error : ' Utilisateur non trouvé ! '});
@@ -28,7 +28,7 @@ exports.login = (req, res, next) => {
                     if (!valid) {
                         return res.status(401).json({ message: 'Mot de passe incorrect !'});
                     }
-                    res.status(201).json({
+                    res.status(200).json({
                         userId: user._id,
                         token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h'}) 
                     });
