6ff104bc7469697a72ace0787bc2d0d2a192dc6a
Jacques Garcia <jack.garcia@outlook.fr>
2020-07-06 08:56:41 +0200

    ajout de dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,25 @@
+const dotenv = require('dotenv').config(); // Protection de l'acces à ma base de donnée
 const express = require('express');// Déclaration framework express
 const bodyParser = require('body-parser');// Déclaration Body-Parser pour récupérer des objets exploitables
 const mongoose = require('mongoose');// Déclaration Mongoose pour base de Données MongoDB
 
-const sauceRoutes = require('./routes/sauce');
+const sauceRoutes = require('./routes/sauce'); // Déclaration du dossier des routes Sauces
 const userRoutes = require('./routes/user');// Déclaration du dossier des routes utilisateur
 
-const sauce = require('./models/Sauce');
-const path = require('path'); //nécessaire pour multer
+const sauce = require('./models/Sauce'); 
+const path = require('path'); //nécessaire pour multer (importation des fichiers)
 
-const app = express();
+const app = express(); 
 
-mongoose.connect('mongodb+srv://JacquesG_OCR:15020710@clustercoursback-6b9sv.mongodb.net/test?retryWrites=true&w=majority',
+//Déclaration base de donnée MongoDB
+mongoose.connect( process.env.MONGO_URL,
     { useNewUrlParser: true,
       useUnifiedTopology: true})
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDb échouée!'));
 
    
-app.use((req, res, next) => {
+app.use((req, res, next) => { //Declaration Cors et Methods
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
